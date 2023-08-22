e6c6fd748b67da647402a28b6b88dfc1a563c493
Rajan Kumar <rajanranjan06254@gmail.com>
2023-08-22 21:51:50 +0530

    first commit

---
+++ footer.php
@@ -0,0 +1,38 @@
+<html>
+<head>
+  <style>
+  #footer {
+
+  position:absolute;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+  height: 3 5px;
+  background-color:#000000;
+  color:white;
+  text-align: center;
+}
+  </style>
+</head>
+<body>
+  <div id="footer" >
+  <b>COPYRIGHT © 
+    <?php
+
+$year = date("Y");
+
+// Display the year
+echo $year;
+
+?>
+
+  Blood Bank & Donation Management
+
+  ALL RIGHTS RESERVED.
+  
+  </div>
+
+
+</body>
+
+</html>
